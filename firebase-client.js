7b54e5830c331f38bcbc6dd5b4a69bb4ecaff91b
Bocko2727 <borislav.iliev.2727@gmail.com>
2026-08-20 14:40:30 +0300

    Update firebase-client.js

--- firebase-client.js
+++ firebase-client.js
@@ -1,28 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, serverTimestamp, query, orderBy } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Legacy Firebase client removed.
+// This module now only provides in-memory access token helpers for Google Drive,
+// and is reserved for future Supabase client integration.
 
-export const firebaseConfig = {
-  projectId: "steel-plexus-x1ttq",
-  appId: "1:560114298135:web:aaa641d4abb079cfbd5b52",
-  apiKey: "AIzaSyBClosp5fjcYIyZ-8fnmLVfy9GhNCVMPgI",
-  authDomain: "steel-plexus-x1ttq.firebaseapp.com",
-  storageBucket: "steel-plexus-x1ttq.firebasestorage.app",
-  messagingSenderId: "560114298135",
-  oAuthClientId: "560114298135-mp0bj1b4c3j0a3n7aor11na4b2eqtral.apps.googleusercontent.com"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-// Configure Google Provider with Drive & Picker scopes
-export const googleAuthProvider = new GoogleAuthProvider();
-googleAuthProvider.addScope('https://www.googleapis.com/auth/drive.file');
-googleAuthProvider.addScope('https://www.googleapis.com/auth/drive.metadata.readonly');
-googleAuthProvider.addScope('https://www.googleapis.com/auth/drive');
-
-// In-memory token management
 let cachedAccessToken = null;
 
 export const setCachedAccessToken = (token) => {
@@ -30,5 +9,3 @@ export const setCachedAccessToken = (token) => {
 };
 
 export const getCachedAccessToken = () => cachedAccessToken;
-
-export { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, serverTimestamp, query, orderBy };
